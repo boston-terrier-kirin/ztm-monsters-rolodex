3010aca519300c78ad75edc16160f91619e8a5a6
boston-terrier-kirin <78889783+boston-terrier-kirin@users.noreply.github.com>
2021-07-16 06:58:50 +0900

    他の人が変更して、コンフリクトが起きる。

--- src/components/search-box/serch-box.js
+++ src/components/search-box/serch-box.js
@@ -1,12 +1,13 @@
 import classes from './search-box.module.css';
 
 export const SearchBox = (props) => {
+	const {placeholder, searchMonster} = props;
 	return (
 		<input
 			className={classes.search}
 			type="search"
-			placeholder={props.placeholder}
-			onChange={props.searchMonster}
+			placeholder={placeholder}
+			onChange={searchMonster}
 		/>
 	);
 };
