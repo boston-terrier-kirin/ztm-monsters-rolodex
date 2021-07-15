62047c09d084a0ecc9ba6361cd992098b936bb45
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2021-07-16 06:56:21 +0900

    パラメータをデスクらクチャリングしました。

--- src/components/search-box/serch-box.js
+++ src/components/search-box/serch-box.js
@@ -1,12 +1,12 @@
 import classes from './search-box.module.css';
 
-export const SearchBox = (props) => {
+export const SearchBox = ({ placeholder, searchMonster }) => {
 	return (
 		<input
 			className={classes.search}
 			type="search"
-			placeholder={props.placeholder}
-			onChange={props.searchMonster}
+			placeholder={placeholder}
+			onChange={searchMonster}
 		/>
 	);
 };
